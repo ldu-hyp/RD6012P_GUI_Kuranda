535f77c092e74f0853da9d50650b696c900f017a
黄玉鹏 <33029976+ldu-hyp@users.noreply.github.com>
2026-09-21 16:46:44 +0800

    Add src/SerialWorker.h

---
+++ src/SerialWorker.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "DeviceData.h"
+
+#include <QByteArray>
+#include <QObject>
+#include <QQueue>
+#include <QSerialPort>
+#include <QTimer>
+
+class SerialWorker final : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit SerialWorker(QObject *parent = nullptr);
+
+public slots:
+    void openPort(const QString &portName);
+    void closePort();
+    void requestImmediatePoll();
+
+    void setVoltage(double volts);
+    void setCurrent(double amps);
+    void setOutputEnabled(bool enabled);
+    void setCurrentRange(int range);
+    void setPollInterval(int milliseconds);
+
+signals:
+    void connectionChanged(bool connected, const QString &message);
+    void deviceInfoReceived(const DeviceInfo &info);
+    void snapshotReceived(const DeviceSnapshot &snapshot);
+    void protocolError(const QString &message);
+    void commandAcknowledged(const QString &message);
+
+private slots:
+    void onReadyRead();
+    void onSerialError(QSerialPort::SerialPortError error);
+    void onRequestTimeout();
+    void onPollTimer();
+
+private:
+    enum class RequestType {
+        DeviceInfo,
+        Brightness,
+        PollState,
+        WriteVoltage,
+        WriteCurrent,
+        WriteOutput,
+        WriteRange
+    };
+
+    struct Request {
+        RequestType type = RequestType::PollState;
+        QByteArray frame;
+        quint8 function = 0;
+        quint16 startRegister = 0;
+        quint16 count = 0;
+        QString description;
+        int retryCount = 0;
+    };
+
+    void enqueueRead(quint16 start, quint16 count, RequestType type,
+                     const QString &description, bool priority = false);
+    void enqueueWrite(quint16 reg, quint16 value, RequestType type,
+                      const QString &description);
+    void runNextRequest();
+    void transmitCurrentRequest();
+    void completeCurrentRequest(const QByteArray &frame);
+    void failCurrentRequest(const QString &reason);
+    void scheduleNextPoll();
+    int expectedFrameLength() const;
+    void tryExtractFrame();
+
+    QSerialPort m_serial;
+    QTimer m_timeoutTimer;
+    QTimer m_pollTimer;
+
+    QByteArray m_rxBuffer;
+    QQueue<Request> m_priorityQueue;
+    QQueue<Request> m_normalQueue;
+
+    Request m_currentRequest;
+    bool m_hasCurrentRequest = false;
+    bool m_connected = false;
+
+    int m_pollIntervalMs = 80;
+    int m_currentRange = 0;
+    int m_lastBacklight = -1;
+
+    static constexpr int kRequestTimeoutMs = 350;
+};

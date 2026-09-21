419aefb610c7d50aad52344ee563a7e9d5ae8433
黄玉鹏 <33029976+ldu-hyp@users.noreply.github.com>
2026-09-21 16:46:49 +0800

    Add src/RealtimePlotWidget.h

---
+++ src/RealtimePlotWidget.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QElapsedTimer>
+#include <QVector>
+#include <QWidget>
+
+class RealtimePlotWidget final : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit RealtimePlotWidget(QWidget *parent = nullptr);
+
+    void addSample(double voltage, double current, double power);
+    void clear();
+    void setTimeWindowSeconds(int seconds);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    struct Sample {
+        qint64 t = 0;
+        double voltage = 0.0;
+        double current = 0.0;
+        double power = 0.0;
+    };
+
+    void trimSamples();
+    void drawLane(QPainter &painter, const QRectF &rect,
+                  const QString &title, const QString &unit,
+                  const QColor &color, int valueSelector,
+                  qint64 nowMs);
+
+    QVector<Sample> m_samples;
+    QElapsedTimer m_clock;
+    int m_timeWindowSeconds = 60;
+};

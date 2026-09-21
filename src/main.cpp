8ab4fc0f6b52ac37e6db99a82aaf2835d8eeeba1
黄玉鹏 <33029976+ldu-hyp@users.noreply.github.com>
2026-09-21 17:15:40 +0800

    Bump version to 0.2.2

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setApplicationName(QStringLiteral("RD6012P GUI Kuranda"));
     app.setOrganizationName(QStringLiteral("Kuranda"));
-    app.setApplicationVersion(QStringLiteral("0.2.1"));
+    app.setApplicationVersion(QStringLiteral("0.2.2"));
 
     QFont font(QStringLiteral("Segoe UI"));
     font.setPointSize(10);
